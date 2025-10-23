9da52bc06f75fee95a5f759095e16a80e7bfe366
gabi1905 <kauane.iankoski@escola.pr.gov.br>
2025-10-23 10:22:29 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
- alert('TEXTE')
+const avanca = document.querySelectorAll (' .bnt-proximo' )
+console.log(avanca)
+
+avanca.forEach(button=> {
+     button.addEventListener('click', function(){ 
+        const atual = document.querySelector('.ativo')
+        const proixmoPasso = 'passo-' + this.getAttribute('data-proximo')
+
+        atual.classList.remove('ativo')
+        document.querElementById(proximoPasso).classList.add('ativo')
+    })
+})
